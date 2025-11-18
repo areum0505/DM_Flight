19798690fee58de19be7519b0238a5053d96b558
areum0505 <areum05055@gmail.com>
2025-11-18 21:52:20 +0900

    중간 보스 1 구현 (오버로드)

--- components/Bullet.js
+++ components/Bullet.js
@@ -1,13 +1,19 @@
 class Bullet {
-  constructor(x, y, speed) {
+  constructor(x, y, speed, vel = null) {
     this.x = x;
     this.y = y;
     this.speed = speed;
     this.size = CONFIG.BULLET.SIZE;
+    this.vel = vel;
   }
 
   move() {
-    this.y += this.speed;
+    if (this.vel) {
+      this.x += this.vel.x;
+      this.y += this.vel.y;
+    } else {
+      this.y += this.speed;
+    }
   }
 
   draw() {
---
+++ configs/bossConfig.js
@@ -0,0 +1,13 @@
+const BOSS_STATS = {
+  OVERLOAD: {
+    HEALTH: 10, // 50
+    SIZE: 100,
+    TURRETS: {
+      HEALTH: 5, // 25
+      SIZE: 40,
+      SHOOT_INTERVAL: 90, // 1.5초
+    },
+    ATTACK_COOLDOWN: 120, // 2초
+    CHARGE_SPEED: 15,
+  }
+};
